ecace08c767753e46a92359355fa028e21b9181a
Jesper Rytter Jensen <jrytterj@hotmail.com>
2018-04-11 18:41:32 +0200

    jj

--- j--src/tests/junit/MultiLinesCommentTest.java
+++ j--src/tests/junit/MultiLinesCommentTest.java
@@ -2,15 +2,15 @@ package junit;
 
 import junit.framework.TestCase;
 
-public class MultiLinesCommentTest extends TestCase {
+public class MultiLinesCommentTest {
 
-	protected void setUp() throws Exception {
-		super.setUp();
-	}
-
-	protected void tearDown() throws Exception {
-		super.tearDown();
-	}
+//	protected void setUp() throws Exception {
+//		super.setUp();
+//	}
+//
+//	protected void tearDown() throws Exception {
+//		super.tearDown();
+//	}
 
 	public void testMultiLinesCommentTest() {
 		/*
